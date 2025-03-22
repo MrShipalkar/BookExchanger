39381ff0b9b018c8038a99e0fb193a0b1e6672aa
Mr.shipalkar <ashwinshipalkar1@gmail.com>
2025-03-22 14:51:58 +0530

    updated fronteend deployed link in backend

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,8 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 
 app.use(cors({
-    origin: "http://localhost:5173", // ✅ Your frontend
+    origin: "https://bookexchanger-west.onrender.com", // ✅ Your frontend
+    // origin: "http://localhost:5173", // ✅ Your frontend
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // ✅ Allow PUT
     credentials: true,
 }));
