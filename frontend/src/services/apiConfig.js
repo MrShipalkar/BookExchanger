c3ff4e4bd67a2cb9ecf9e22b1faf73c52ae7772e
Mr.shipalkar <ashwinshipalkar1@gmail.com>
2025-03-22 14:49:37 +0530

    fixed UI for books page, orders sorting

--- frontend/src/services/apiConfig.js
+++ frontend/src/services/apiConfig.js
@@ -3,7 +3,11 @@ import axios from "axios";
 
 
 // services/apiConfig.js
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://bookexchanger-backend.onrender.com";
+
+// use this link for local machine run
+// const BASE_URL = "http://localhost:5000";
+
 
 const instance = axios.create({
     baseURL: `${BASE_URL}/api`,
